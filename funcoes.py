5de70740cdde84761e90c1ef5b69b88ac8b1482c
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-05-23 09:15:05 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,6 @@
+def exibe_bom_dia():
+    print('Bom dia')
+    print('Tudo bom')
+
+exibe_bom_dia()
+exibe_bom_dia()
---
+++ funcoes2.py
@@ -0,0 +1,24 @@
+def soma(n1, n2):
+    resultado = n1 + n2
+    print(resultado)
+
+def soma(n1, n2, n3):
+    resultado = n1 + n2 + n3
+    print(resultado)
+
+def soma(n1, n2, n3):
+    resultado = n1 + n2 + n3
+    return resultado
+
+def multiplica(n1, n2, n3):
+    resultado = n1 * n2 * n3
+    return resultado
+
+soma_total = soma(5, 2, 10)
+multiplica_total = multiplica(5, 2, 10)
+
+soma(5, 2)
+soma(5, 2, 10)
+print(soma_total)
+print(multiplica_total)
+
